5f0882d587886d2e60a1ecff18204e3e27473e9e
twp3379 <wkakzks@naver.com>
2021-05-27 16:28:01 +0900

    2021.05.27

---
+++ WebContent/META-INF/sql/SELECT01.sql
@@ -0,0 +1,15 @@
+SELECT CustomerName, City FROM Customers;
+SELECT * FROM Customers;
+SELECT customername FROM Customers;
+SELECT Customername FROM customers;
+select customername from Customers;
+SELECT Customername FROM Customers;
+-- 명령문들은 대문자, 소문자 구분하지 않는다.
+SELECT * FROM Categories; -- 8
+SELECT * FROM Customers; -- 91
+SELECT * FROM Employees; -- 10
+SELECT * FROM OrderDetails; -- 518
+SELECT * FROM Orders; -- 196
+SELECT * FROM Products; -- 77
+SELECT * FROM Shippers; -- 3
+SELECT * FROM Suppliers; -- 29
