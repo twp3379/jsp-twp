fb1d4a482e2454f3c1780d89e0b5e72feef63472
twp3379 <wkakzks@naver.com>
2021-05-30 19:19:39 +0900

    0528

---
+++ src/ch14/bean/Employee.java
@@ -0,0 +1,26 @@
+package ch14.bean;
+
+public class Employee {
+	private int id; 
+	private String lastname;
+	private String firstname;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+}
