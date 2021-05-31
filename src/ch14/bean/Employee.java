18dc6f2ec5de0c320da1908550f6fad70859496a
twp3379 <wkakzks@naver.com>
2021-05-31 21:34:53 +0900

    0531

--- src/ch14/bean/Employee.java
+++ src/ch14/bean/Employee.java
@@ -1,26 +1,35 @@
 package ch14.bean;
 
 public class Employee {
-	private int id; 
-	private String lastname;
-	private String firstname;
+	private int id;
+	private String lastName;
+	private String firstName;
+	private String notes;
+	
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	public String getNotes() {
+		return notes;
+	}
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getLastname() {
-		return lastname;
+	public String getLastName() {
+		return lastName;
 	}
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
-	public String getFirstname() {
-		return firstname;
+	public String getFirstName() {
+		return firstName;
 	}
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
-
+	
+	
 }
